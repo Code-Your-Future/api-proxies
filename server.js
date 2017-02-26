23dbd7dc843cd97e9b5f964954156bcf9e72fae5
Rhys Evans <wheresrhys@gmail.com>
2017-02-26 18:11:09 +0000

    forward link headers

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const githubClient = require('./lib/github-client');
 app.get('*', (req, res) => {
 	if (process.env.PROXY_TO === 'github') {
 		return githubClient.fetchUrl(req.originalUrl)
-			.then(json => {
+			.then((json, link) => {
 				Object.keys(json).forEach(k => {
 					if (/_url$/.test(k)) {
 						json[k] = json[k].replace('https://api.github.com', 'http://cyf-github-api.herokuapp.com')
 					}
 				})
+				if (link) {
+					res.set('link', link)
+				}
 				res.json(json)
 			})
 			.catch(err => {
